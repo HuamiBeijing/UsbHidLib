94c181d9e875c4f9b90c885ae1b1c9f34e3f3f6a
EIva <astellar@gmail.com>
2014-08-26 23:37:32 +0400

    Initial HID communication.

---
+++ UsbHidLib/Device.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32.SafeHandles;
+
+namespace UsbHidLib
+{
+   public class Device : IDisposable
+   {
+
+      /* device handle */
+      private IntPtr handle;
+      /* stream */
+      private FileStream _fileStream;
+
+      /* stream */
+      public FileStream fileStream
+      {
+         get { return _fileStream; }
+         internal set { _fileStream = value; }
+      }
+
+      /* dispose */
+      public void Dispose()
+      {
+         /* deal with file stream */
+         if (_fileStream != null)
+         {
+            /* close stream */
+            _fileStream.Close();
+            /* get rid of object */
+            _fileStream = null;
+         }
+
+         /* close handle */
+         Native.CloseHandle(handle);
+      }
+
+      /* open hid device */
+      public Device(string path)
+      {
+         /* safe file handle */
+         SafeFileHandle shandle;
+
+         /* opens hid device file */
+         handle = Native.CreateFile(path,
+             Native.GENERIC_READ | Native.GENERIC_WRITE,
+             Native.FILE_SHARE_READ | Native.FILE_SHARE_WRITE,
+             IntPtr.Zero, Native.OPEN_EXISTING, Native.FILE_FLAG_OVERLAPPED,
+             IntPtr.Zero);
+
+         /* whops */
+         if (handle == Native.INVALID_HANDLE_VALUE)
+         {
+            throw new Exception("Could not open HID device");
+         }
+
+         /* build up safe file handle */
+         shandle = new SafeFileHandle(handle, false);
+
+         /* prepare stream - async */
+         _fileStream = new FileStream(shandle, FileAccess.ReadWrite,
+             32, true);
+
+      }
+
+      /* write record */
+      public void Write(byte[] data)
+      {
+         /* write some bytes */
+         _fileStream.Write(data, 0, data.Length);
+         /* flush! */
+         _fileStream.Flush();
+      }
+
+      /* read record */
+      public void Read(byte[] data)
+      {
+         /* get number of bytes */
+         int n = 0, bytes = data.Length;
+
+         /* read buffer */
+         while (n != bytes)
+         {
+            /* read data */
+            int rc = _fileStream.Read(data, n, bytes - n);
+            /* update pointers */
+            n += rc;
+         }
+      }
+   }
+}
---
+++ UsbHidTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsbHidLib;
+
+namespace UsbHidTest
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var devs = Browser.Browse();
+         Console.WriteLine(String.Join("\n", devs.Select(info=>String.Format("VID={0:X4}, PID={1:X4}, Product={2}", info.Vid, info.Pid, info.Product))));
+
+         var devInfo = devs.Last();
+
+         using (var dev = new Device(devInfo.Path))
+         {
+            var data = new byte[5];
+            dev.Read(data);
+            Console.WriteLine(String.Join(" ", data));
+         }
+      }
+
+   }
+}
